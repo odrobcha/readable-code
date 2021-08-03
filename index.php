4e6b740049f2817fbe69a3923262b2d103024240
oksanka <drobchak.o@gmail.com>
2021-08-03 12:07:29 +0200

    Code refactoring

--- index.php
+++ index.php
@@ -1,85 +1,69 @@
 <?php
 
-// fw = for who
-function ordr_pz($pizzatype, $fw) {
-
-    $type = $pizzatype;
-    echo 'Creating new order... <br>';
-    $toPrint = 'A ';
-    $toPrint .= $pizzatype;
-    $p = calc_cts($type);
-
-    $address = 'unknown';
-    if($fw == 'koen')
-    {
+$orders = [
+    ['pizza'=>'calzone', 'person'=>'koen'],
+    ['pizza'=>'margarita', 'person'=>'manuele'],
+    ['pizza'=>'golden', 'person'=>'students'],
+];
+function orderPizza($pizzaType, $person) {
+    try {
+        $price = getPrice($pizzaType);
+    }
+    catch (Exception $e){
+        echo 'Exception: ' . $e->getMessage(); //handle the exception
+    }
+    if($person == 'koen'){
         $address = 'a yacht in Antwerp';
-    } elseif ($fw == 'manuele')
-    {
+    }
+    if ($person == 'manuele'){
         $address = 'somewhere in Belgium';
-    } elseif ($fw == 'students') {
+    }
+    if ($person == 'students') {
         $address = 'BeCode office';
     }
 
-    $toPrint .=   ' pizza should be sent to ' . $fw . ". <br>The address: {$address}.";
-    echo $toPrint; echo '<br>';
-    echo'The bill is €'.$p.'.<br>';
-
-
-
-
-    echo "Order finished.<br><br>";
+    echo 'Creating new order... <br>'
+            .'A ' . $pizzaType  .' pizza should be sent to ' . $person . ". <br>The address:" .$address
+            .'<br>'
+            .'The bill is €' .$price .'.<br>'
+            ."Order finished .<br><br>";
 }
-
-function total_price($p) {
-    return $p;
-}
-
-function test($p_type) {
-    echo "Test: type is {$p_type}. <br>";
-}
-
-function calc_cts($p_type)
+/**
+ * Return pizza price
+ *
+ * @throws Exception
+ */
+function getPrice($pizza)
 {
-    $cst = 'unknown';
+/*    $pizzas = [
+        'margarita' => 5,
+        'golden' => 100,
+        'calzone' => 10
+    ];
 
-    if ($p_type == 'marguerita') {
-        $cst = 5;
-    }
-    else
-    {
-        if ($p_type == 'golden')
-        {
-            $cst = 100;
-        }
-
-        if ($p_type == 'calzone')
-        {
-            $cst = 10;
-        }
-
-        if ($p_type == 'hawai') {
-            throw new Exception('Computer says no');
-        }
+    if(!in_array($pizzaType, $pizzas)){
+        throw new Exception($pizza . 'pizza is not in menu list');
     }
 
-    return $cst;
+    return $pizzas[$pizzaType];
+
+*/
+   switch($pizza) {
+        case 'margarita':
+            return 5;
+        case 'golden':
+            return 100;
+        case 'calzone':
+            return 10;
+        default:
+            throw new Exception($pizza . 'pizza is not in menu list');
+    }
 }
 
-function ordr_piz_all()
+function orderPizzaToAll($orderList)
 {
-    $test= 0;
-    ordr_pz('calzone', 'koen');
-    ordr_pz('marguerita', 'manuele');
-
-    ordr_pz('golden', 'students');
-}
-
-function make_Allhappy($do_it) {
-    if ($do_it) {
-        ordr_piz_all();
-    } else {
-        // Should not do anything when false
+   foreach ($orderList as $order){
+      orderPizza($order['pizza'], $order['person']);
     }
 }
-
-make_Allhappy(true);
+orderPizzaToAll($orders);
